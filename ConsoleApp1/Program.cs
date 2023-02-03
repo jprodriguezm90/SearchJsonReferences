c232a30959085e3d3fa3439474bd837f48a1b0b3
Juan Pablo Rodriguez Montoya <jprodriguezm90@gmail.com>
2023-02-03 01:07:34 -0500

    03-02-2023 Object ComponentJson to Create

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -18,20 +18,24 @@ namespace ConsoleApp1
             var enumerable = l.AsEnumerable();
 
             var item = enumerable.GetEnumerator();
-            var result = "";
+            List<ComponentJson> result = null;
 
             while (item.MoveNext())
             {
                 var itemc = item.Current;
-                result += searchWord(itemc, "www.zoetisus.com");
+                result = searchWord(itemc, result, "xxx");
             }
             Console.ReadLine();
 
         }
 
-        private static string searchWord(JToken itemc, string searchKey)
+        private static List<ComponentJson> searchWord(JToken itemc, List<ComponentJson> result, string searchKey)
         {
-            var result = "";
+            if (result == null)
+            {
+                result = new List<ComponentJson>();
+            }
+
             var childrens = itemc.Children();
             if (childrens.ToList().Count == 0)
             {
@@ -48,22 +52,46 @@ namespace ConsoleApp1
                     var i = itemChildrens.First();
                     if (i.HasValues)
                     {
-                        result += searchWord(enumerator.Current, searchKey);
+                        result = searchWord(enumerator.Current, result, searchKey);
                     }
                     else
                     {
-                        var values = item.ToArray();
-                        if (values != null && values.Length > 0)
-                        {
-                            var found = values[0].ToString().Contains(searchKey);
-                            if (found)
-                            {
-                                result += item.Parent.ToString();
-                            }
-                        }
+                        result = BrowserLeaves(searchKey, result, item);
                     }
                 }
+                else
+                {
+                    result = BrowserLeaves(searchKey, result, item);
+                }
+
             }
+            return result;
+        }
+
+        private static List<ComponentJson> BrowserLeaves(string searchKey, List<ComponentJson> result, JToken item)
+        {
+            var values = item.ToArray();
+            if (values != null && values.Length > 0)
+            {
+                var found = values[0].ToString().Contains(searchKey);
+                if (found)
+                {
+                    var parent = item.Parent.Path;
+                    var parentstr = parent.Substring(parent.LastIndexOf('.') + 1);
+
+                    result.Add(new ComponentJson(parentstr,item.Parent.ToString(),item.ToString()));
+                }
+            }
+            else
+            {
+                var found = item.ToString().Contains(searchKey);
+                if (found)
+                {
+                    result.Add(new ComponentJson(item.Parent.Path, item.Parent.ToString(), item.ToString()));
+                }
+            }
+
+
             return result;
         }
 
@@ -72,6 +100,23 @@ namespace ConsoleApp1
             string fs = File.ReadAllText("json.txt");
 
             return fs;
+        }
+    }
+
+    public class ComponentJson
+    {
+        public string Root { get; set; }
+        public string Context { get; set; }
+        public string Item { get; set; }    
+        public ComponentJson(string root, string context, string item) 
+        {
+            Root = root;    
+            Context = context;
+            Item = item;
+        }
+        public override string ToString()
+        {
+            return $"{Root} : {Context} : {Item}";
         }
     }
 }
